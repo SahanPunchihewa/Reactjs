6b7070d1f2e74c28000040e0645a8f7672748431
Sahan Punchihewa <sahannilupul18@gmail.com>
2021-12-30 23:12:00 +0530

    React

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to React';
+  const likes = 50;
+  const link = 'https://www.google.com';
+  
+
   return (
+
     <div className="App">
       
     <div className="content">
-    <h1>App Component</h1>
+    <h1>{title}</h1>
+    <p>Likes {likes} Times</p>
+    
+    <p>{ 10 }</p>
+    <p>{ "Hello World"}</p>
+    <p>{[1,2,3,4,5]}</p>
+    <p>{ Math.random() *10}</p>
+      
+    <a href={link}>Google</a>
 
     </div>
     </div>
