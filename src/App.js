a01c5084c2b70d2ecb92e8664abcabfed8a1ac25
Sahan Punchihewa <sahannilupul18@gmail.com>
2021-12-31 00:12:43 +0530

    React

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-
-  const title = 'Welcome to React';
-  const likes = 50;
-  const link = 'https://www.google.com';
-  
 
   return (
 
     <div className="App">
-      
+
+      <Navbar/>
     <div className="content">
-    <h1>{title}</h1>
-    <p>Likes {likes} Times</p>
     
-    <p>{ 10 }</p>
-    <p>{ "Hello World"}</p>
-    <p>{[1,2,3,4,5]}</p>
-    <p>{ Math.random() *10}</p>
-      
-    <a href={link}>Google</a>
+    <Home/>
 
     </div>
     </div>
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+const Navbar = () => {
+    return (
+
+      <nav className="navbar">
+
+        <h1>Welcome to React</h1>
+
+        <div className="link">
+            
+            <a href = "/">Home</a>
+            <a href = "/create">New Blog</a>
+            </div>    
+
+      </nav>  
+
+
+    );
+}
+ 
+export default Navbar;
