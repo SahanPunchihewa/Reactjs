574de0bcea5bed7090ab633d8cd539844ab37ff8
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-01-01 22:20:07 +0530

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
 const Home = () => {
+
+    const handleClick = () => {
+
+        console.log('Hello React');
+
+    }
+
     return (  
 
         <div className="home">
 
         <h2>HomePage</h2>
+        <button onClick ={handleClick}>Click Me</button>
 
         </div>
 
