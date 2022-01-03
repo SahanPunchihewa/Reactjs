6060fc7075b4b69af6a1f6902d19a826cbbf2a7f
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-01-04 00:01:07 +0530

    useState

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,15 @@
+import {useState} from 'react';
+
 const Home = () => {
+
+    const [name, setName] = useState('Sahan');
+    const [age, setAge] = useState(24);
+
 
     const handleClick = (e) => { // Click Event
 
-        console.log('Hello React', e);
+        setName('Sahan Punchihewa');
+        //console.log('Hello React', e);
     }
     const handleClickAgain = (name, e) => { // Click Event
 
@@ -14,7 +21,9 @@ const Home = () => {
         <div className="home">
 
         <h2>HomePage</h2>
+        <p>{name} is {age} Years Old</p>
         <button onClick ={handleClick}>Click Me</button> {/* Handle Click function invoke */ }
+        <br/>
         <button onClick ={(e)=> handleClickAgain('Sahan', e)}>Click Me Again</button> {/* Handle Click again function invoke */ }
 
         </div>
